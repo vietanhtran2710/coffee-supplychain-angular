16b6571f327ea5fbccfdd0dd4895a8e6784378c3
vietanhtran2710 <vietanhtran.uet@gmail.com>
2021-11-15 01:31:10 +0700

    add User component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AdminComponent } from './admin/admin.component';
+import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
   {path: "", component: HomeComponent}, 
-  {path: "admin", component: AdminComponent}
+  {path: "admin", component: AdminComponent},
+  {path: "user", component: UserComponent}
 ];
 
 @NgModule({
