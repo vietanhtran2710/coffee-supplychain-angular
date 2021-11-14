cd98212f923a3ad04952877c2c76e82b0c9a85ec
vietanhtran2710 <vietanhtran.uet@gmail.com>
2021-11-15 01:10:52 +0700

    add Admin component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
 
 const routes: Routes = [
-  {path: "", component: HomeComponent}
+  {path: "", component: HomeComponent}, 
+  {path: "admin", component: AdminComponent}
 ];
 
 @NgModule({
