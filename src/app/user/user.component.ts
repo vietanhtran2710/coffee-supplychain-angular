6e630d8a7f3b72fc203808bd6bb49191af59d86e
vietanhtran2710 <vietanhtran.uet@gmail.com>
2021-11-15 14:30:53 +0700

    display single user info

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import Moralis from 'moralis';
 import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from '../services/supplyUser/user.service';
 
 @Component({
   selector: 'app-user',
@@ -9,10 +10,26 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class UserComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  userInfo = {
+    address: '',
+    name: '',
+    contact: '',
+    role: ''
+  }
+
+  constructor(private router: Router,
+              private userService: UserService) { }
 
   ngOnInit(): void {
-
+    let that = this;
+    let currentAddress = Moralis.User.current().get('ethAddress');
+    this.userService.getUserDetail(currentAddress, currentAddress)
+    .then(function (result) {
+      that.userInfo.address = currentAddress;
+      that.userInfo.contact = (result as any).contactNo;
+      that.userInfo.name = (result as any).name;
+      that.userInfo.role = (result as any).role;
+    })
   }
 
   logOut() {
